10b40b74842f3c2afb2b47ade1f0306e05a3fa7a
Sayan Samanta <sayans0323@gmail.com>
2024-05-20 20:28:25 +0530

    Hello World!! using C

---
+++ Hello_World_Using_C/helloWorld.c
@@ -0,0 +1,9 @@
+#include<stdio.h>                 // <-- Header File for printf();
+#include<conio.h>                 // <-- Header File for getch();
+
+int main()                        // <-- main() function, code start execution from here.
+{                                 // <-- Body of main() function start from here.
+    printf("Hello World!!\n");    // <-- printf() also a function. It print ("....") on the screen. '\n' used to move the cursor to the next line.
+    getch();                      // <-- getch() is a function which get a character. We use it here to puse the code after printf(). To see the output.
+    return 0;                     // <-- main funtion return 0. It means that the program executed successfully.
+}                                 // <-- main() body end.
