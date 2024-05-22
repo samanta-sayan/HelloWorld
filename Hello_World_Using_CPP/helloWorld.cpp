a2ee1c054bf792f0a9b0f88d18a0a2273673534e
Sayan Samanta <sayans0323@gmail.com>
2024-05-22 22:01:47 +0530

    Add Hello World!! using CPP

---
+++ Hello_World_Using_CPP/helloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>              // <-- For cout<<
+#include<conio.h>               // <-- Header File for getch();
+
+using namespace std;            // <-- For cout<<
+
+int main()                      // <-- main() function, code start execution from here.
+{                               // <-- Body of main() function start from here.
+    cout<<"Hello World!!\n";    // <-- cout<<"__"; use to print. '\n' use to move the cursor to the next line.
+    getch();                    // <-- getch() is a function which get a character. We use it here to pause the code after 'cout'. To see the output.
+    return 0;                   // <-- main() funtion return 0. It means that the program executed successfully.
+}                               // <--  main() body end.
